8c3ca1e4fb418a85a42d95ae73a758839fd65f37
cagatay yilmaz <mustafa.cagatay.yilmaz@gmail.com>
2025-01-13 22:18:08 +0300

    Created test instance object example

--- src/test/java/io/cagatay/MethodsOrderedByOrderIndex.java
+++ src/test/java/io/cagatay/MethodsOrderedByOrderIndex.java
@@ -1,34 +1,43 @@
 package io.cagatay;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//@TestInstance(TestInstance.Lifecycle.PER_METHOD) this is default settings
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MethodsOrderedByOrderIndex {
+    StringBuilder executed = new StringBuilder();
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("executed is "+ executed);
+    }
 
     @Order(3)
     @Test
     void testB(){
         System.out.println("Running test B");
+        executed.append("4");
     }
 
     @Test
     @Order(2)
     void testA(){
         System.out.println("Running test A");
+        executed.append("2");
     }
 
     @Order(1)
     @Test
     void testD(){
         System.out.println("Running test D");
+        executed.append("1");
     }
 
-    @Order(3)
+    @Order(4)
     @Test
     void testC(){
         System.out.println("Running test C");
+        executed.append("3");
     }
 }
